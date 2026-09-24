c21ef10fc404f918e85c4bc408d37bd30fcabe4d
Thales <thales.augusto.chaves98@gmail.com>
2026-09-24 00:13:20 -0300

    .

--- script/app.js
+++ script/app.js
@@ -1,13 +1,16 @@
-const newListBtn = document.getElementById("new-list-btm");
-const marketsBtn = document.getElementById("markets-btn");
-const shoppingListsBtn = document.getElementById("shopping-lists-btn");
+const mainScreen = document.querySelector("main");
+const newListBtn = document.querySelector(".new-list-btn");
+const newMarketBtn = document.querySelector(".markets-btn");
 
 
-marketsBtn.addEventListener("click", () => {
-    document.querySelector(".markets").classList.remove("hidden");
-    document.querySelector(".shopping-lists").classList.add("hidden");
-});
-shoppingListsBtn.addEventListener("click", () => {
-    document.querySelector(".shopping-lists").classList.remove("hidden");
-    document.querySelector(".markets").classList.add("hidden");
+
+mainScreen.addEventListener("click", (e) =>{
+    let targetBtn = e.target.document.querySelector("button");
+    console.log(targetBtn);
+
+    if(targetBtn.contains("new-list-btn")){
+        newListBtn.classList.remove("hidden");
+    } else if(targetBtn.contains("markets-btn")){
+        newMarketBtn.classList.remove("hidden");
+    }
 });
